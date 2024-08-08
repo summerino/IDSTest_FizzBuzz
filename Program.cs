faadb8dbc57dc8a33a1c570fa2d73791ce7c7592
Galang Bilal <galang.bilal@sahassa.co.id>
2024-08-08 16:42:20 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Insert the number: ");
+        int desiredNumber = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(FizzBuzz(desiredNumber)); 
+    }
+
+    private static string FizzBuzz(int value)
+    {
+        if (value % 5 == 0 && value % 3 == 0) {
+            return "FizzBuzz";
+        }
+        else if (value % 5 == 0)
+        {
+            return "Buzz";
+        }
+        else if (value % 3 == 0)
+        {
+            return "Fizz";
+        }
+        else
+        {
+            return value.ToString();
+        }
+    }
+}
